f0329c70f8a2a5319d2dea41eee22e8e65bdb0d3
joyfive <joy_five@kakao.com>
2025-07-24 22:18:42 +0900

    style: console.log 추가

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -1,20 +1,28 @@
 // /app/api/login/route.js (Next.js 13+)
-import { cookies } from "next/headers"
-import { NextResponse } from "next/server"
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  const cookieStore = await cookies()
-  const LOGIN_TOKEN = cookieStore.get("LOGIN_TOKEN")?.value
-  const auth_date = cookieStore.get("auth_date")?.value
-  const token = process.env.LOGIN_TOKEN
+  const cookieStore = await cookies();
+  const LOGIN_TOKEN = cookieStore.get("LOGIN_TOKEN")?.value;
+  const auth_date = cookieStore.get("auth_date")?.value;
+  const token = process.env.LOGIN_TOKEN;
   const cookieData = {
     LOGIN_TOKEN: LOGIN_TOKEN,
     auth_date: auth_date,
-  }
+  };
   if (cookieData.LOGIN_TOKEN === token) {
+    console.log(
+      "Invalid token or no token found",
+      "cookieData:",
+      cookieData,
+      "token:",
+      token,
+      cookieData.LOGIN_TOKEN === token
+    );
     return new NextResponse(JSON.stringify({ ok: true, cookieData }), {
       status: 200,
-    })
+    });
   } else {
     console.log(
       "Invalid token or no token found",
@@ -23,9 +31,9 @@ export async function GET() {
       "token:",
       token,
       cookieData.LOGIN_TOKEN === token
-    )
+    );
     return new NextResponse(JSON.stringify({ ok: false }), {
       status: 401,
-    })
+    });
   }
 }
